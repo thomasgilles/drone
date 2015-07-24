54fdae39e7fb923065896e7600832adc6910efe9
Thomas <thomasgilleslycee@gmail.com>
2015-07-24 10:54:05 +0100

    i like dolphins

--- drone-backend.js
+++ drone-backend.js
@@ -12,11 +12,41 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav", {
+        driver: "ardrone-nav",
+        connection: "ardrone"
+    })
     .on("ready", fly);
-    
+
 // Fly the bot
 function fly(robot) {
+    bot = robot;
+    bot.drone.disableEmergency();
 
+    bot.drone.config('general:navdata_demo', 'TRUE');
+
+    bot.nav.on("navdata", function(data) {
+        console.log(data);
+    });
+
+    bot.drone.ftrim();
+    bot.drone.takeoff();
+
+    after(5 * 1000, function() {
+        bot.drone.up(0.2);
+    });
+
+    after(6.5 * 1000, function() {
+        bot.drone.left(0.1);
+    });
+
+    after(2*1000, function() {
+        bot.drone.land();
+    });
+
+    after(15*1000, function() {
+        bot.drone.stop();
+    });
 }
 
 Cylon.start();
